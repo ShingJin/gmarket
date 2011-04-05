40c6d5821bb9a78a8b4f870f9fdd7bf5fe3c37d6
grin_itstaff <grin_itstaff@ma2855.grinnell.edu>
2011-04-04 20:42:03 -0500

    per_page

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
 
 
   def index
-      @items = Item.category(params[:category]).search(params[:search],params[:bs]).order(sort_column + ' ' + sort_direction).paginate(:per_page =>9, :page => params[:page])  
+      @items = Item.category(params[:category]).search(params[:search],params[:bs]).order(sort_column + ' ' + sort_direction).paginate(:per_page =>20, :page => params[:page])  
   end
 
   def show	
