53e063f66b09a03baeed617547c7184a5a508430
grin_itstaff <grin_itstaff@ma2855.grinnell.edu>
2011-04-11 21:32:29 -0500

    Add show all

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,8 @@ class ItemsController < ApplicationController
 
   def index
       @items = Item.category(params[:category]).search(params[:search],params[:bs]).order(sort_column + ' ' + sort_direction).paginate(:per_page =>20, :page => params[:page])  
-  end
+ 	  @categories=Category.all
+ end
 
   def show	
     @item = Item.find(params[:id])
