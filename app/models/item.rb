f48bead3c653048a15092e72ccea164497fde2b2
grin_itstaff <grin_itstaff@ma2855.grinnell.edu>
2011-04-12 12:42:22 -0500

    missing image

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,6 +16,7 @@ class Item < ActiveRecord::Base
   
 
    has_attached_file :picture,
+	    :default_url => "/images/missing.png"
     	:styles => {
       	:thumb=> "100x100#",
       	:small  => "300x300>" },
