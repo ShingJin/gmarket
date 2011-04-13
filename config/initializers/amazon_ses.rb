1ba5e7c15e787b98c93c21a08e1e40a6f51f2c01
grin_itstaff <grin_itstaff@ma2855.grinnell.edu>
2011-04-13 16:20:04 -0500

    mailer

--- config/initializers/amazon_ses.rb
+++ config/initializers/amazon_ses.rb
@@ -1,3 +1,3 @@
 ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => ENV['AKIAICDXU5SXRWQA5RQA'],
-  :secret_access_key => ENV['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']
+  :access_key_id     => 'AKIAICDXU5SXRWQA5RQA',
+  :secret_access_key => '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'
