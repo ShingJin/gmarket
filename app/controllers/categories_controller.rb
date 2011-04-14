b3ec272b492ec4e56b4df85de31c0a6c63128068
grin_itstaff <grin_itstaff@ma2855.grinnell.edu>
2011-04-13 19:06:05 -0500

    per page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
    helper_method :sort_column, :sort_direction
 
   def show
-      @items = Item.where("category_id =?",params[:id]).category(params[:category]).search(params[:search],params[:bs]).order(sort_column + ' ' + sort_direction).paginate(:per_page =>20, :page => params[:page])  
+      @items = Item.where("category_id =?",params[:id]).category(params[:category]).search(params[:search],params[:bs]).order(sort_column + ' ' + sort_direction).paginate(:per_page =>100, :page => params[:page])  
  	  @categories=Category.all
   end
 
