f2b11c73de676fbfc07c77575e3ae440faab77aa
grin_itstaff <grin_itstaff@ma2855.grinnell.edu>
2011-04-14 19:55:08 -0500

    something

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,9 +1,13 @@
 class Notifier < ActionMailer::Base
   default :from => "238357@gmail.com"
   
-  def notify
+  def notify(user)
+    @user = user
+    @url="http://www.thegmarket.me"
     mail(:to => "jinxin@grinnell.edu",
-         :subject => "Someone commmented on your Item")
+         :subject => "Someone commmented on your Item") do |format|
+      	  format.html { render 'notify_email' }
+    end
   	
   	
   end
