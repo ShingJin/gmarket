89cd1638ccb5f409abd1b6be1f5b67b30c3eccd0
grin_itstaff <grin_itstaff@ma2855.grinnell.edu>
2011-04-16 14:34:41 -0500

    something

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Marketplace::Application.routes.draw do |map|
 
 
 	
-  resources :items
+  map.resources :items, :member=>{:notify=>:get}
   resources :users
   resources :categories
   resources :autocomplete_searches, :only => [:index], :as => 'autocomplete'
