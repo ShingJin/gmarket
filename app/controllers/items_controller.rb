38fe7567c1d63e810284dd235f0be3195239d9f8
grin_itstaff <grin_itstaff@ma2855.grinnell.edu>
2011-04-15 20:25:50 -0500

    something

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,6 +6,7 @@ class ItemsController < ApplicationController
   def index
       @items = Item.category(params[:category]).search(params[:search],params[:bs]).order(sort_column + ' ' + sort_direction).paginate(:per_page =>30, :page => params[:page])  
  	  @categories=Category.all
+
  end
 
   def show	
@@ -18,10 +19,6 @@ class ItemsController < ApplicationController
 
   end
 
-  def notify
-    @item = Item.find(params[:id])
-  	redirect_to items_url
-  end
 
 
 
