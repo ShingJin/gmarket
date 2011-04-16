f94e14d039479adf0ed317bcfd789cc1da6f106e
grin_itstaff <grin_itstaff@ma2855.grinnell.edu>
2011-04-15 20:14:31 -0500

    something

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -19,7 +19,7 @@ class ItemsController < ApplicationController
   def notify
     @item = Item.find(params[:id])
   	Notifier.notify(User.find(@item.user_id))
-  	redirect_to '/'
+  	redirect_to items_url
   end
 
 
