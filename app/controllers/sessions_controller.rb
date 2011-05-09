af4e12e565bca3d748f82b81e20256fe5f2f0502
grin_itstaff <grin_itstaff@ma2855.grinnell.edu>
2011-05-08 21:01:14 -0500

    publish

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
    auth = request.env["omniauth.auth"]  
    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)  
    session[:user_id] = user.id
-   session["credentials"]["token"]=auth["credentials"]["token"]
+   session[:token]=auth["credentials"]["token"]
    if user.phone.nil?|user.email.nil?
       flash[:success]= "Welcome, please fill out this contact form so that buyers can reach you"
 	  redirect_to edit_user_path(current_user)
