596e00683f73eeca8dc8dbda41402c631e79e3f0
grin_itstaff <grin_itstaff@ma2855.grinnell.edu>
2011-05-08 21:10:17 -0500

    publish

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do  
-	 provider :facebook	, '154290874631361', 'e76ef2433a59b01bbec53bf0012b4cc1'  
+	 provider :facebook	, '154290874631361', 'e76ef2433a59b01bbec53bf0012b4cc1',:scope => 'publish_stream,user_likes,friends_likes,email,offline_access'
 end  
