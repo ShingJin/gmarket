445112a3736282c0306eac7cd434a8d5cb59f5c0
unknown <dwayne@.(none)>
2011-08-25 06:40:15 -0500

    dfd

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,10 @@ class Item < ActiveRecord::Base
   
   
   validates :name,  :presence   => true, :length     => { :maximum => 200 }
-  validates_numericality_of :price, :message=>"is not a number"
-  validates_inclusion_of :price, :in => 0..99999
+  #validates_numericality_of :price, :message=>"is not a number"
+  #validates_inclusion_of :price, :in => 0..99999
   validates :description, :length     => { :maximum => 400 }
 
-  
   
   belongs_to :category	
   belongs_to :subcategory
